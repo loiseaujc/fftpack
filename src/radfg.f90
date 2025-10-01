f8d8df9051e997c319617682c9bb25e9f089ff87
loiseaujc <loiseau.jc@gmail.com>
2025-10-01 19:30:46 +0000

    Deploying to fftpack-fpm-ilp64 from @ loiseaujc/fftpack@52276124a0e0afbd947ffdaa346f689358167582 🚀

--- src/radfg.f90
+++ src/radfg.f90
@@ -21,15 +21,17 @@
          idp2 = ido + 2
          nbd = (ido - 1)/2
          if (ido == 1) then
-            do concurrent(ik=1:idl1)
+            do ik = 1, idl1
                c2(ik, 1) = ch2(ik, 1)
             end do
          else
-            do concurrent(ik=1:idl1)
+            do ik = 1, idl1
                ch2(ik, 1) = c2(ik, 1)
             end do
-            do concurrent(j=2:ip, k=1:l1)
-               ch(1, k, j) = c1(1, k, j)
+            do j = 2, ip
+               do k = 1, l1
+                  ch(1, k, j) = c1(1, k, j)
+               end do
             end do
             if (nbd > l1) then
                is = -ido
@@ -63,27 +65,37 @@
                end do
             end if
             if (nbd < l1) then
-               do concurrent(i=3:ido:2, j=2:ipph, k=1:l1)
+               do j = 2, ipph
                   jc = ipp2 - j
-                  c1(i - 1, k, j) = ch(i - 1, k, j) + ch(i - 1, k, jc)
-                  c1(i - 1, k, jc) = ch(i, k, j) - ch(i, k, jc)
-                  c1(i, k, j) = ch(i, k, j) + ch(i, k, jc)
-                  c1(i, k, jc) = ch(i - 1, k, jc) - ch(i - 1, k, j)
+                  do i = 3, ido, 2
+                     do k = 1, l1
+                        c1(i - 1, k, j) = ch(i - 1, k, j) + ch(i - 1, k, jc)
+                        c1(i - 1, k, jc) = ch(i, k, j) - ch(i, k, jc)
+                        c1(i, k, j) = ch(i, k, j) + ch(i, k, jc)
+                        c1(i, k, jc) = ch(i - 1, k, jc) - ch(i - 1, k, j)
+                     end do
+                  end do
                end do
             else
-               do concurrent(i=3:ido:2, j=2:ipph, k=1:l1)
+               do j = 2, ipph
                   jc = ipp2 - j
-                  c1(i - 1, k, j) = ch(i - 1, k, j) + ch(i - 1, k, jc)
-                  c1(i - 1, k, jc) = ch(i, k, j) - ch(i, k, jc)
-                  c1(i, k, j) = ch(i, k, j) + ch(i, k, jc)
-                  c1(i, k, jc) = ch(i - 1, k, jc) - ch(i - 1, k, j)
+                  do k = 1, l1
+                     do i = 3, ido, 2
+                        c1(i - 1, k, j) = ch(i - 1, k, j) + ch(i - 1, k, jc)
+                        c1(i - 1, k, jc) = ch(i, k, j) - ch(i, k, jc)
+                        c1(i, k, j) = ch(i, k, j) + ch(i, k, jc)
+                        c1(i, k, jc) = ch(i - 1, k, jc) - ch(i - 1, k, j)
+                     end do
+                  end do
                end do
             end if
          end if
-         do concurrent(j=2:ipph, k=1:l1)
+         do j = 2, ipph
             jc = ipp2 - j
-            c1(1, k, j) = ch(1, k, j) + ch(1, k, jc)
-            c1(1, k, jc) = ch(1, k, jc) - ch(1, k, j)
+            do k = 1, l1
+               c1(1, k, j) = ch(1, k, j) + ch(1, k, jc)
+               c1(1, k, jc) = ch(1, k, jc) - ch(1, k, j)
+            end do
          end do
 !
          ar1 = 1.0_dp
@@ -93,7 +105,7 @@
             ar1h = dcp*ar1 - dsp*ai1
             ai1 = dcp*ai1 + dsp*ar1
             ar1 = ar1h
-            do concurrent(ik=1:idl1)
+            do ik = 1, idl1
                ch2(ik, l) = c2(ik, 1) + ar1*c2(ik, 2)
                ch2(ik, lc) = ai1*c2(ik, ip)
             end do
@@ -106,51 +118,67 @@
                ar2h = dc2*ar2 - ds2*ai2
                ai2 = dc2*ai2 + ds2*ar2
                ar2 = ar2h
-               do concurrent(ik=1:idl1)
+               do ik = 1, idl1
                   ch2(ik, l) = ch2(ik, l) + ar2*c2(ik, j)
                   ch2(ik, lc) = ch2(ik, lc) + ai2*c2(ik, jc)
                end do
             end do
          end do
-         do concurrent(ik=1:idl1, j=2:ipph)
-            ch2(ik, 1) = ch2(ik, 1) + c2(ik, j)
+         do j = 2, ipph
+            do ik = 1, idl1
+               ch2(ik, 1) = ch2(ik, 1) + c2(ik, j)
+            end do
          end do
 !
          if (ido < l1) then
-            do concurrent(i=1:ido, k=1:l1)
-               cc(i, 1, k) = ch(i, k, 1)
+            do i = 1, ido
+               do k = 1, l1
+                  cc(i, 1, k) = ch(i, k, 1)
+               end do
             end do
          else
-            do concurrent(i=1:ido, k=1:l1)
-               cc(i, 1, k) = ch(i, k, 1)
+            do k = 1, l1
+               do i = 1, ido
+                  cc(i, 1, k) = ch(i, k, 1)
+               end do
             end do
          end if
-         do concurrent(j=2:ipph, k=1:l1)
+         do j = 2, ipph
             jc = ipp2 - j
             j2 = j + j
-            cc(ido, j2 - 2, k) = ch(1, k, j)
-            cc(1, j2 - 1, k) = ch(1, k, jc)
+            do k = 1, l1
+               cc(ido, j2 - 2, k) = ch(1, k, j)
+               cc(1, j2 - 1, k) = ch(1, k, jc)
+            end do
          end do
          if (ido == 1) return
          if (nbd < l1) then
-            do concurrent(i=3:ido:2, j=2:ipph, k=1:l1)
+            do j = 2, ipph
                jc = ipp2 - j
                j2 = j + j
-               ic = idp2 - i
-               cc(i - 1, j2 - 1, k) = ch(i - 1, k, j) + ch(i - 1, k, jc)
-               cc(ic - 1, j2 - 2, k) = ch(i - 1, k, j) - ch(i - 1, k, jc)
-               cc(i, j2 - 1, k) = ch(i, k, j) + ch(i, k, jc)
-               cc(ic, j2 - 2, k) = ch(i, k, jc) - ch(i, k, j)
+               do i = 3, ido, 2
+                  ic = idp2 - i
+                  do k = 1, l1
+                     cc(i - 1, j2 - 1, k) = ch(i - 1, k, j) + ch(i - 1, k, jc)
+                     cc(ic - 1, j2 - 2, k) = ch(i - 1, k, j) - ch(i - 1, k, jc)
+                     cc(i, j2 - 1, k) = ch(i, k, j) + ch(i, k, jc)
+                     cc(ic, j2 - 2, k) = ch(i, k, jc) - ch(i, k, j)
+                  end do
+               end do
             end do
          else
-            do concurrent(i=3:ido:2, j=2:ipph, k=1:l1)
+            do j = 2, ipph
                jc = ipp2 - j
                j2 = j + j
-               ic = idp2 - i
-               cc(i - 1, j2 - 1, k) = ch(i - 1, k, j) + ch(i - 1, k, jc)
-               cc(ic - 1, j2 - 2, k) = ch(i - 1, k, j) - ch(i - 1, k, jc)
-               cc(i, j2 - 1, k) = ch(i, k, j) + ch(i, k, jc)
-               cc(ic, j2 - 2, k) = ch(i, k, jc) - ch(i, k, j)
+               do k = 1, l1
+                  do i = 3, ido, 2
+                     ic = idp2 - i
+                     cc(i - 1, j2 - 1, k) = ch(i - 1, k, j) + ch(i - 1, k, jc)
+                     cc(ic - 1, j2 - 2, k) = ch(i - 1, k, j) - ch(i - 1, k, jc)
+                     cc(i, j2 - 1, k) = ch(i, k, j) + ch(i, k, jc)
+                     cc(ic, j2 - 2, k) = ch(i, k, jc) - ch(i, k, j)
+                  end do
+               end do
             end do
          end if
       end subroutine radfg
